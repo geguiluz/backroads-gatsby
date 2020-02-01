1bb0b0d8810afb0d674d19e2ec4af867fb6fceeb
geguiluz <memo.eguiluz@gmail.com>
2020-01-31 19:03:37 -0600

    Inline styling example

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,8 @@ export default () => (
   <Layout>
     Hello world!
     <Link to="/blog">Blog</Link>
+    <h1 style={{ fontSize: "20px", textTransform: "capitalize", color: "red" }}>
+      hello styling
+    </h1>
   </Layout>
 )

3e4c59f9837f6f1ff671be645e6f560ec7a36d61
geguiluz <memo.eguiluz@gmail.com>
2020-01-31 23:25:44 -0600

    Footer finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,42 @@
 import React from "react"
+import styles from "../css/footer.module.css"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import { Link } from "gatsby"
+import { FaProject } from "react-icons/fa"
 
 const Footer = () => {
   return (
     <div>
-      <h1>I'm a Footer</h1>
+      <footer className={styles.footer}>
+        <div className={styles.links}>
+          {links.map((item, index) => {
+            return (
+              <Link key={index} to={item.url}>
+                {item.text}
+              </Link>
+            )
+          })}
+        </div>
+        <div className={styles.icons}>
+          {socialIcons.map((item, index) => {
+            return (
+              <a
+                key={index}
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.icon}
+              </a>
+            )
+          })}
+        </div>
+        <div className={styles.copyright}>
+          Copyright &copy; Backroads Travel Co. {new Date().getFullYear()} all
+          rights reserved
+        </div>
+      </footer>
     </div>
   )
 }

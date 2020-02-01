1f5c5dc11f09dacfe03b3a808ef7f01d3407ab2a
geguiluz <memo.eguiluz@gmail.com>
2020-01-31 19:50:33 -0600

    Implementing sass

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Fragment } from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import "./layout.css"
 
 export default ({ children }) => (
   <Fragment>

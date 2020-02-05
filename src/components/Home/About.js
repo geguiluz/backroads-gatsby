8f321ecc5e7310de74b75da85621d61ed9a266e3
geguiluz <memo.eguiluz@gmail.com>
2020-02-04 18:54:07 -0600

    About section completed

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,12 +1,37 @@
 import React from "react"
 import Title from "../Title"
+import styles from "../../css/about.module.css"
+import img from "../../images/defaultBcg.jpeg"
 
 const About = () => {
   return (
-    <div>
-      Hello fom About
+    <section className={styles.about}>
       <Title title="about" subtitle="us" />
-    </div>
+      {/* Classes in css moudules are hyphened, but are exported as CamelCase */}
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="About Company" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>Explore The difference</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            veniam placeat dolores ullam ad saepe tenetur beatae illum alias
+            doloremque?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            veniam placeat dolores ullam ad saepe tenetur beatae illum alias
+            doloremque?
+          </p>
+          <button type="button" className="btn-primary">
+            Learn More
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 

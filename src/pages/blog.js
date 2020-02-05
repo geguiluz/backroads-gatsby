5f71e48b736e49d9bf8499f04be63424a8109f0e
geguiluz <memo.eguiluz@gmail.com>
2020-02-04 22:01:16 -0600

    Trying out page queries

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,32 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
-
-export default () => {
-  return <Layout>Hello from blog page</Layout>
+import { graphql } from "gatsby"
+const blog = ({ data }) => {
+  return (
+    <Layout>
+      Hello from blog page!!!
+      <Link to="/">Back Home</Link>
+      <h1>Title: {data.site.siteMetadata.title}</h1>
+    </Layout>
+  )
 }
+
+// This is a page query that can only be used within this page
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        data {
+          name
+          age
+        }
+      }
+    }
+  }
+`
+
+export default blog

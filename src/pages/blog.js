4eb6e7c07037da447f6690dde1d0cd8f7b53a1b3
geguiluz <memo.eguiluz@gmail.com>
2020-02-04 22:08:41 -0600

    Fixing blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,11 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
-return (
-  <Layout>
-    Hello from blog page!!!
-    <Link to="/">Back Home</Link>
-  </Layout>
-)
-
+const blog = () => {
+  return (
+    <Layout>
+      Hello from blog page!!!
+      <Link to="/">Back Home</Link>
+    </Layout>
+  )
+}
 export default blog

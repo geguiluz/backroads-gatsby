f59de932504b3ebd1731526e20a9b739f14b1f80
geguiluz <memo.eguiluz@gmail.com>
2020-02-14 22:33:31 -0600

    Creating days collapsible component using state

--- src/components/SingleTour/Day.js
+++ src/components/SingleTour/Day.js
@@ -1,7 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
+import { FaAngleDown } from "react-icons/fa"
+import styles from "../../css/day.module.css"
 
-function Day() {
-  return <div>Hello from Days</div>
+function Day({ day, info }) {
+  const [showInfo, setInfo] = useState(false)
+  const toggleInfo = () => {
+    setInfo(showInfo => !showInfo)
+  }
+  return (
+    <article className={styles.day}>
+      <h4>
+        {day}
+        <button className={styles.btn} onClick={toggleInfo}>
+          <FaAngleDown />
+        </button>
+      </h4>
+      {showInfo && <p>{info}</p>}
+    </article>
+  )
 }
 
 export default Day
